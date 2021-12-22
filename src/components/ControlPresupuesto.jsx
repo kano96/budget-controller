d8b8169127739ece2182f99e1d395e84412d924e
Kevin Torres <kevin_akd@hotmail.com>
2021-12-21 22:10:08 -0500

    resuelto disponible y gastado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const ControlPresupuesto = ({ presupuesto }) => {
+const ControlPresupuesto = ({ gastos, presupuesto }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0
+    );
+    const totalDisponible = presupuesto - totalGastado;
+    setGastado(totalGastado);
+    setDisponible(totalDisponible);
+  }, [gastos]);
+
   const formatearCantidad = (cantidad) => {
     return cantidad.toLocaleString("en-US", {
       style: "currency",
       currency: "USD",
     });
   };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
@@ -17,10 +31,10 @@ const ControlPresupuesto = ({ presupuesto }) => {
           <span>Presupuesto:</span> {formatearCantidad(presupuesto)}
         </p>
         <p>
-          <span>Disponible:</span> {formatearCantidad(0)}
+          <span>Disponible:</span> {formatearCantidad(disponible)}
         </p>
         <p>
-          <span>Gastado:</span> {formatearCantidad(0)}
+          <span>Gastado:</span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
